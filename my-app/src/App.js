24056692ef7f0eb16c032ec912abbddcac547663
deathstar250 <maciel.galeano@anima.edu.uy>
2018-10-16 10:34:57 -0300

    comit loco

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react'; 
-import logo from './logo.svg'; 
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Ejemplo from './components/ejemplo/ejemplo';
 import Listar from './components/ejemplo/listar';
 import Tablero from './components/snake/Tablero';
 import Casilla from './components/snake/Casilla';
+import Perfil from './components/props-childs/Perfil';
 
 class App extends Component {
   render() {
+    var nombre = "maciel";
+    nombre += " como estas";
+    var numero = 2;
+    numero += 2;
+
     return (
       <div>
+        <Perfil nombre={nombre}/>
+        <Perfil nombre={numero}/>
         <Tablero>
         </Tablero>
       </div>
-      
+
     );
   }
 }
---
+++ my-app/src/components/props-childs/Perfil.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Perfil extends Component {
+    render() {
+      return (
+        <div>
+          {this.props.nombre}
+        </div>
+      );
+  }
+}
+export default Perfil;
